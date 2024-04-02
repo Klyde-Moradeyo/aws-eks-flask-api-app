ba73f3fa560063da9e8d7b4e97ec6d7c3ee33248
Klyde <klyde.moradeyo.dev@gmail.com>
2024-04-02 16:33:05 +0100

    Fix flask api app

--- py/app.py
+++ py/app.py
@@ -1,15 +1,22 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask
+from views import init_app_routes
+from extensions import db, migrate
 import os
 
+# Load the environment variables from the .env file
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "FLASK_DATABASE_URL", "sqlite:///test.db"
+)
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    with app.app_context():
+        db.create_all()
+
+    init_app_routes(app, db)
+    app.run(debug=True, host='0.0.0.0')
